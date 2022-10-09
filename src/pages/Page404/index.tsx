124e8f0e9093bc2b9fc162f893e1918d8c5e2ba2
liuqi <liuqi@neurongenius.com>
2022-10-09 10:54:39 +0800

    feat: 404 diy

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,16 +1,25 @@
 /*
- * @Author: liu7i 
- * @Date: 2022-04-13 17:03:11 
- * @Last Modified by:   liu7i 
- * @Last Modified time: 2022-04-13 17:03:11 
+ * @Author: liu7i
+ * @Date: 2022-04-13 17:03:11
+ * @Last Modified by: liu7i
+ * @Last Modified time: 2022-09-28 10:40:18
  */
 
-import React from 'react';
+import React from "react";
+import moment from "moment";
+import "./index.scss";
 
 const Page = () => {
-  return <div>
-    1
-  </div>;
+  return (
+    <div>
+      404
+      <div className="box">
+        <div className="item1"></div>
+        <div className="item2"></div>
+        <div className="item3"></div>
+      </div>
+    </div>
+  );
 };
 
-export default Page
+export default Page;
